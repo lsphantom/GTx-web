9127a8ec7c466a05c7d53adf87b27ac531851ba0
Gary <gp@limestripes.us>
2019-08-20 17:53:24 -0600

    init outline

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>BUILD OUTLINE</h3>
+      <ul>
+        <li>Splash Screen
+            <ul>
+              <li>Create Account</li>
+              <li>Sign In</li>
+            </ul>
+        </li>
+        <li>My Garage
+          <ul>
+            <li>Add New Vehicle</li>
+            <li>My Vehicle
+              <ul>
+                <li>Photos</li>
+                <li>Specs</li>
+                <li>Maintenance</li>
+                <li>Parts</li>
+                <li>Car Settings</li>
+                <li>* More (Glovebox)</li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+
+      </ul>
     </div>
   );
 }
